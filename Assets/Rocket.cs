81e9387f1d28c70978908dc74114163c23a63831
rick <rickd1977@gmail.com>
2018-03-01 15:32:41 +0900

    Rocket Built

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+        if (Input.GetKey(KeyCode.Space))   // Can thrust while rotating.
+        {
+            print("Thrust activated.");
+        }
+
+        
+    }
+}
